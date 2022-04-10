879ef4428bd6b3da411c3dcbb7e7eb12b36f066a
MahbubTonoy <tnymrbadboy@gmail.com>
2022-04-10 22:53:11 +0600

    build: added landing page

---
+++ resources/views/Frontend/landing/partials/js.blade.php
@@ -0,0 +1,10 @@
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{asset("Frontend/landing/ven/easing/easing.min.js")}}"></script>
+    <script src="{{asset("Frontend/landing/ven/isotope/isotope.pkgd.min.js")}}"></script>
+    <script src="{{asset("Frontend/landing/ven/lightbox/js/lightbox.min.js")}}"></script>
+
+    <!-- Template Javascript -->
+    <script src="{{asset("Frontend/landing/res/js/main.js")}}"></script>
