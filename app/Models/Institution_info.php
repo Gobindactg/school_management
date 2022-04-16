49a63194759abc1fa7566cf08909074f8f16eefd
MahbubTonoy <tnymrbadboy@gmail.com>
2022-04-16 23:39:02 +0600

    fix: updated git conflict

--- app/Models/Institution_info.php
+++ app/Models/Institution_info.php
@@ -7,38 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Institution_info extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-=======
->>>>>>> a422376adac50beb3dadb138a419272829d768be
     public function parent()
     {
         return $this->belongsTo(Upazila::class);
-        
     }
     public function user()
     {
-      return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> a422376adac50beb3dadb138a419272829d768be
     public function division()
     {
         return $this->belongsTo(Division::class);
     }
     public function district()
-        {
-            return $this->belongsTo(District::class);
-        }
+    {
+        return $this->belongsTo(District::class);
+    }
     public function upazila()
-        {
-            return $this->belongsTo(Upazila::class);
-        }
-<<<<<<< HEAD
-   
-
-=======
->>>>>>> a422376adac50beb3dadb138a419272829d768be
+    {
+        return $this->belongsTo(Upazila::class);
+    }
 }
