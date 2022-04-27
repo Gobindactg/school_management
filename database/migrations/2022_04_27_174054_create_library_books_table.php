0ab2a32da32fdae403fa8cd14b2d1708a1d44c31
MahbubTonoy <tnymrbadboy@gmail.com>
2022-04-27 23:53:42 +0600

    build: added book adding options

---
+++ database/migrations/2022_04_27_174054_create_library_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('library_books', function (Blueprint $table) {
+            $table->id();
+            $table->integer("insitittion_id");
+            $table->string("book_name");
+            $table->string("author");
+            $table->integer("category_id");
+            $table->integer("quantity");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('library_books');
+    }
+};
